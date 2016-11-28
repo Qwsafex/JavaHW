c1bafdcca511a5afbc4f91c36919fcc56a15e226
Ilya <scorpioni4@yandex.ru>
2016-11-29 00:41:28 +0300

    SecondPartTasks implemented

--- TestDriven/src/main/java/sp/SecondPartTasks.java
+++ TestDriven/src/main/java/sp/SecondPartTasks.java
@@ -1,7 +1,12 @@
 package sp;
 
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public final class SecondPartTasks {
 
@@ -9,25 +14,56 @@ public final class SecondPartTasks {
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths.stream()
+                    .flatMap(path -> {
+                        try{
+                            return Files.lines(Paths.get(path));
+                        }
+                        catch (IOException e) {
+                            return null;
+                        }
+                    })
+                    .filter(line -> line.contains(sequence))
+                    .collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
     // Стрелок атакует мишень и каждый раз попадает в произвольную точку квадрата.
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать, какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        final int ITERATIONS = 1000000;
+        Random random = new Random();
+        double radius = 0.5;
+        return IntStream
+                .iterate(1, n -> n + 1)
+                .limit(ITERATIONS)
+                .filter(i -> {
+                    double x = random.nextDouble() * 2 * radius - radius;
+                    double y = random.nextDouble() * 2 * radius - radius;
+                    return x * x + y * y < radius * radius;
+                })
+                .count() / (.0 + ITERATIONS);
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions.entrySet()
+                           .stream()
+                           .max(Comparator.comparing(entry -> entry.getValue()
+                                                                   .stream()
+                                                                   .mapToInt(String::length)
+                                                                   .sum())).get().getKey();
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        BiConsumer<Map<String, Integer>, Map<String, Integer>> combine = (map1, map2) -> {
+            map2.forEach((key, val) -> {
+                map1.compute(key, (k,v) -> (v == null) ? val : val + v);
+            });
+        };
+        return orders.stream().collect(HashMap::new, combine, combine);
     }
 }
